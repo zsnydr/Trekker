3615121379ea04f414a51ec404f2142805d2e13e
zsnydr <zackwsnyder@gmail.com>
2016-10-11 18:55:54 -0700

    Altered layout/styles

--- client/public/app.js
+++ client/public/app.js
@@ -1,5 +1,4 @@
 
-
 $(document).on('ready', function() {
 
   var kelvinToFar = function(kelvin) {
@@ -12,55 +11,60 @@ $(document).on('ready', function() {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
   var getData = function(city, cb) {
-  $.ajax({
-    type: 'POST',
-    url: 'http://localhost:3000/city',
-    data: JSON.stringify({city: city}),
-    contentType: 'application/json',
-  })
-  .done(function(results) {
-    console.log('AJAX RESULTS ', JSON.parse(results));
-    cb(JSON.parse(results));
-  });
-};
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/city',
+      data: JSON.stringify({city: city}),
+      contentType: 'application/json',
+    })
+    .done(function(results) {
+      console.log('AJAX RESULTS ', JSON.parse(results));
+      cb(JSON.parse(results));
+    });
+  };
   var buildDestination = function(results) {
+    var weather = JSON.parse(results.weather);
+    var temp = kelvinToFar(weather.main.temp);
+    var desc = capString(weather.weather[0].description);
+    var $dest = $('.dest');
+
+    var $miles = $('<div class="miles"></div><span>' + addCommas(results.d) + ' miles</span>');
+
+    var $loc = $('<div class="loc">' + $dest.val() + '</div>');
+    var $temp = $('<div class="temp">Current temp: ' + temp + '°F</div>');
+    var $desc = $('<div class="desc">' + desc + '</div>');
+    var $city = $('<div class="city"></div>').append($loc).append($temp).append($desc);
+
+    if (!first) {
+      $itin.append($miles);
+      totalMiles += Number(results.d);
+      $('.total').html('');
+      $('.total').append($('<div>Total Miles: ' + addCommas(totalMiles) + '</div>'));
+    }
+
+    $itin.append($city);
+    $itin[0].scrollTop = $itin[0].scrollHeight;
+    $dest.val('');
+    first = false;
 
   };
 
   var totalMiles = 0;
   var first = true;
+  var $itin = $('.itin');
 
-  $(".new-dest").click(function(){
-
-    var $itin = $('.itin');
-    var $dest = $('.dest');
-
-    getData($dest.val(), function(results) {
-
-      var weather = JSON.parse(results.weather);
-      var temp = kelvinToFar(weather.main.temp);
-      var desc = capString(weather.weather[0].description);
-
-      var $miles = $('<div class="miles">' + addCommas(results.d) + ' miles</div>');
-
-      var $loc = $('<div class="loc">' + $dest.val() + '</div>');
-      var $temp = $('<div class="temp">Current temp: ' + temp + '°F</div>');
-      var $desc = $('<div class="desc">' + desc + '</div>');
-      var $city = $('<div class="city"></div>').append($loc).append($temp).append($desc);
-
-      if (!first) {
-        $itin.append($miles);
-      }
-
-      $itin.append($city);
-      $itin[0].scrollTop = $itin[0].scrollHeight;
-      $dest.val('');
-      first = false;
-
-      totalMiles += Number(results.d);
-      $('.total').html('');
-      $('.total').append($('<div>Total Miles: ' + addCommas(totalMiles) + '</div>'));
-
+  $(".new-dest").click(function() {
+    getData($('.dest').val(), function(results) {
+      buildDestination(results);
     });
   });
+
+  $(".clear-all").click(function() {
+    $itin.html('');
+    first = true;
+    totalMiles = 0;
+    $('.total').html('');
+    $('.total').append($('<div>Total Miles: ' + addCommas(totalMiles) + '</div>'));
+  });
+
 });
